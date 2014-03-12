cc78643031b91d82bc343f8e87d7691972261890
shannah <steve@weblite.ca>
2014-03-12 16:34:19 -0700

    Initial commit

---
+++ src/ca/weblite/codename1/netbeans/Installer.java
@@ -0,0 +1,118 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ca.weblite.codename1.netbeans;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URL;
+import org.apache.commons.io.FileUtils;
+
+import org.netbeans.api.project.Project;
+import org.netbeans.api.project.ProjectManager;
+import org.openide.filesystems.FileObject;
+import org.openide.filesystems.FileUtil;
+import org.openide.util.EditableProperties;
+import org.openide.util.Mutex;
+import org.openide.util.MutexException;
+
+/**
+ *
+ * @author shannah
+ */
+public class Installer {
+    public static final int version = 3;
+    public static final String VERSION_KEY = "cn1-ios-local.version";
+    public static final String PROPERTIES_PATH = "nbproject/project.properties";
+    private final Project project;
+    
+    public Installer(Project proj){
+        this.project = proj;
+    }
+    
+    public void update() throws IOException{
+        EditableProperties props = getEditableProperties(project,  PROPERTIES_PATH);
+        String oldVersionStr = props.get(VERSION_KEY);
+        int currentVersion = -1;
+        if ( oldVersionStr != null ){
+            try {
+                currentVersion = Integer.parseInt(oldVersionStr);
+            } catch ( NumberFormatException ex){
+                
+            }
+        }
+        if ( currentVersion < version){
+            updateBuildScript();
+            props.setProperty(VERSION_KEY, ""+version);
+            storeEditableProperties(project, PROPERTIES_PATH, props);
+        }
+        
+    }
+    
+    private void updateBuildScript() throws IOException{
+        URL buildScript = this.getClass().getResource("build-ios-local.xml");
+        File projectDir = FileUtil.toFile(project.getProjectDirectory());
+        File nbproject = new File(projectDir, "nbproject");
+        File buildScriptDest = new File(nbproject, "build-ios-local.xml");
+        FileUtils.copyURLToFile(buildScript, buildScriptDest);
+    }
+    
+    private  EditableProperties getEditableProperties(final Project prj,final  String propertiesPath)
+        throws IOException {
+        try {
+            return
+            ProjectManager.mutex().readAccess(new Mutex.ExceptionAction<EditableProperties>() {
+                @Override
+                public EditableProperties run() throws IOException {
+                    FileObject propertiesFo = prj.getProjectDirectory().getFileObject(propertiesPath);
+                    EditableProperties ep = null;
+                    if (propertiesFo!=null) {
+                        InputStream is = null;
+                        ep = new EditableProperties(false);
+                        try {
+                            is = propertiesFo.getInputStream();
+                            ep.load(is);
+                        } finally {
+                            if (is != null) {
+                                is.close();
+                            }
+                        }
+                    }
+                    return ep;
+                }
+            });
+        } catch (MutexException ex) {
+            return null;
+        }
+    }
+
+    private static void storeEditableProperties(final Project prj, final  String propertiesPath, final EditableProperties ep)
+        throws IOException {
+        try {
+            ProjectManager.mutex().writeAccess(new Mutex.ExceptionAction<Void>() {
+                @Override
+                public Void run() throws IOException {
+                    FileObject propertiesFo = prj.getProjectDirectory().getFileObject(propertiesPath);
+                    if (propertiesFo!=null) {
+                        OutputStream os = null;
+                        try {
+                            os = propertiesFo.getOutputStream();
+                            ep.store(os);
+                        } finally {
+                            if (os != null) {
+                                os.close();
+                            }
+                        }
+                    }
+                    return null;
+                }
+            });
+        } catch (MutexException ex) {
+        }
+    }
+}
---
+++ src/ca/weblite/codename1/netbeans/actions/BuildIOSLocally.java
@@ -0,0 +1,165 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ca.weblite.codename1.netbeans.actions;
+
+import ca.weblite.codename1.netbeans.Installer;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.beans.PropertyChangeListener;
+import java.io.File;
+import java.io.IOException;
+import java.util.Collection;
+import java.util.Properties;
+import javax.swing.AbstractAction;
+import javax.swing.Action;
+import org.apache.tools.ant.module.api.support.ActionUtils;
+import org.netbeans.api.project.Project;
+import org.openide.DialogDisplayer;
+import org.openide.NotifyDescriptor;
+import org.openide.awt.ActionID;
+import org.openide.awt.ActionReference;
+import org.openide.awt.ActionReferences;
+import org.openide.awt.ActionRegistration;
+import org.openide.execution.ExecutorTask;
+import org.openide.filesystems.FileObject;
+import org.openide.filesystems.FileUtil;
+import org.openide.util.ContextAwareAction;
+import org.openide.util.Exceptions;
+import org.openide.util.Lookup;
+import org.openide.util.LookupEvent;
+import org.openide.util.LookupListener;
+import org.openide.util.NbBundle.Messages;
+import org.openide.util.Task;
+import org.openide.util.TaskListener;
+import org.openide.util.Utilities;
+import org.openide.util.WeakListeners;
+
+@ActionID(
+        category = "Build",
+        id = "ca.weblite.codename1.netbeans.actions.BuildIOSLocally"
+)
+@ActionRegistration(
+        displayName = "#CTL_BuildIOSLocally"
+)
+@ActionReferences({
+    @ActionReference(path = "Menu/BuildProject", position = -90),
+    @ActionReference(path = "Loaders/folder/any/Actions", position = 0),
+    @ActionReference(path="Projects/org-netbeans-modules-java-j2seproject/Actions",  position=791)
+})
+@Messages("CTL_BuildIOSLocally=Build for IOS Locally")
+public final class BuildIOSLocally extends AbstractAction implements ContextAwareAction, LookupListener {
+
+    private final Lookup lkp;
+    private final Lookup.Result<Project> result;
+    
+
+    public BuildIOSLocally(){
+        this(Utilities.actionsGlobalContext());
+    }
+    
+    public BuildIOSLocally(Lookup lookup) {
+        super(Bundle.CTL_BuildIOSLocally());
+        this.lkp = lookup;
+        result = this.lkp.lookupResult(Project.class);
+        result.addLookupListener(
+                WeakListeners.create(LookupListener.class, this, result)
+        );
+        
+        
+        
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        for ( Project p : result.allInstances()){
+            ContextAction action = new ContextAction(p);
+            action.actionPerformed(e);
+        }
+    }
+    
+    private static class ContextAction extends AbstractAction {
+
+        private final Project context;
+        
+        ContextAction(Project context){
+            this.context = context;
+        }
+        
+        @Override
+        public void actionPerformed(ActionEvent ev) {
+            try {
+                // TODO use context
+
+                Installer installer = new Installer(context);
+                installer.update();
+
+                File buildDir = new File(FileUtil.toFile(context.getProjectDirectory()), "build");
+                File iosDir = new File(buildDir, "ios");
+                if ( !iosDir.exists() ){
+                    NotifyDescriptor nd = new NotifyDescriptor(
+                            "No Xcode project has been created yet.  Do you want to create it now?", 
+                            "Create Xcode Project?",
+                            NotifyDescriptor.YES_NO_OPTION,
+                            NotifyDescriptor.QUESTION_MESSAGE,
+                            null,
+                            NotifyDescriptor.YES_OPTION
+                    );
+                    if ( DialogDisplayer.getDefault().notify(nd) == NotifyDescriptor.YES_OPTION ){
+                        final FileObject buildScript = context.getProjectDirectory()
+                                .getFileObject("nbproject")
+                                .getFileObject("build-ios-local.xml");
+
+                        ExecutorTask task = ActionUtils.runTarget(buildScript, new String[]{"clean-xcode-project"}, null);
+                        
+
+                    }
+                } else {
+                    final FileObject buildScript = context.getProjectDirectory()
+                                .getFileObject("nbproject")
+                                .getFileObject("build-ios-local.xml");
+
+                        ExecutorTask task = ActionUtils.runTarget(buildScript, new String[]{"build-for-ios-locally"}, null);
+                        
+                }
+                
+
+                        
+                        
+
+            } catch (IOException ex) {
+                Exceptions.printStackTrace(ex);
+            }
+
+        }
+    }
+
+    @Override
+    public Action createContextAwareInstance(Lookup lkp) {
+        return new BuildIOSLocally(lkp);
+    }
+
+   
+
+    
+
+   
+
+    @Override
+    public void resultChanged(LookupEvent le) {
+        Collection<? extends Project> sel = result.allInstances();
+        if ( sel.size() > 0 ){
+            Project p = sel.toArray(new Project[0])[0];
+            FileObject projectDir = p.getProjectDirectory();
+            FileObject cn1PropertiesFile = projectDir.getFileObject("codenameone_settings.properties");
+            if ( cn1PropertiesFile != null ){
+                super.setEnabled(true);
+                return;
+            }
+        }
+        
+        super.setEnabled(false);
+    }
+}
